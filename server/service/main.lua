c34854d894d549fc4be7b1f4f83ac83be2080535
ndhisrfzs <hissorrow@sina.com>
2016-06-30 01:11:07 -0700

    change file path

---
+++ server/service/main.lua
@@ -0,0 +1,14 @@
+local skynet = require "skynet"
+
+skynet.start(function()
+	local loginserver = skynet.newservice("logind")
+	local gate = skynet.newservice("gated", loginserver)
+
+	skynet.newservice("database")
+
+	skynet.call(gate, "lua", "open" , {
+		port = 8888,
+		maxclient = 64,
+		servername = "sample",
+	})
+end)
